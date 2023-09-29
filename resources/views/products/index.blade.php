8ac9ac3582f07dc067029d33af4d426d86f21910
JelleCurio <jellevandenbeemt@gmail.com>
2023-09-29 12:42:14 +0200

    awd

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,6 +12,9 @@
 						@unless(empty($product->description))
 							<p>{{ $product->description }}</p>
 						@endunless
+						@unless(empty($product->discount))
+							<p class="discount">Nu <span>{{ $product->discount }}%</span> korting!</p>
+						@endunless
 					</div>
 					<button class="btn btn-primary">Meer info &amp; bestellen</button>
 				</div>
